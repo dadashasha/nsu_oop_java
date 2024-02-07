d19a5c3d2a726f937358e0fdec171ab1c830d636
dadashasha <d.ovcharenko@g.nsu.ru>
2024-02-08 01:49:17 +0700

    добавление методов для пользователя и улучшение механизма работы игры

--- Task1_Bulls_And_Cows/src/main/java/ru/nsu/dovcharenko/BullsAndCowsGame.java
+++ Task1_Bulls_And_Cows/src/main/java/ru/nsu/dovcharenko/BullsAndCowsGame.java
@@ -3,43 +3,48 @@ package ru.nsu.dovcharenko;
 import java.util.Scanner;
 
 public class BullsAndCowsGame {
-    private int[] computerGuessedNumber; // Задуманное компьютером число
-    private int[] userEnteredNumber; // Введенное пользователем число
-    private int numberLength; //Длинна массива для чисел
+    private int[] computerGuessedNumber; // задуманное компьютером число
+    private int[] userEnteredNumber; // введенное пользователем число
+    private int numberLength; // длинна массива для чисел
 
-    private void printCompNum(int[] computerGuessedNumber){ // Потом убрать
+    private void printCompNum(int[] computerGuessedNumber){ // потом убрать, нужно чтобы следить за работой кода
         for (int i = 0; i < numberLength; ++i){
             System.out.print(computerGuessedNumber[i] + " ");
         }
     }
 
+    // ввод пользователем длины числа
     private void getCompNum() {
         Scanner scanner = new Scanner(System.in); // Scanner - класс в Java, System.in - стандартный поток ввода
         do {
-            System.out.println("Введите число от 1 до 10");
+            System.out.println("Введите длину числа от 1 до 10");
             while (!scanner.hasNextInt()){ //.hasNextInt() - метод, проверяющий следующий ввод на целочисленность
                 System.out.println("Введите число");
                 scanner.next(); // .next() - метод, к. считывает следующее слово (строку без пробеллов из потока ввода)
             }
             numberLength = scanner.nextInt(); // .nextInt() - считывание целого числа
-        } while (numberLength <= 0 || numberLength > 10);
+        } while (numberLength <= 0 || numberLength > 10); // ввод производится, пока не будет вверная длина от 1 до 10
 
-        setComputerGuessedNumber(createRandArray());
+        setComputerGuessedNumber(createRandArray()); // если длина введена правильно, то создается рандомное число
     }
 
     public void setComputerGuessedNumber(int[] computerGuessedNumber){
         this.computerGuessedNumber = computerGuessedNumber;
     }
 
+    // генерация числа
     private int[] createRandArray(){
         int[] tempArr = new int[numberLength];
         for (int i = 0; i < this.numberLength; ++i){
-            tempArr[i] = (int) (Math.random() * 10);
+            tempArr[i] = (int) (Math.random() * 10); // заполнение случайными числами
         }
 
         // Проверка на повторение цифр
         boolean checkArr = false;
         while (!checkArr){
+            if (tempArr.length == 1){
+                break;
+            }
             for (int i = 0; i < tempArr.length; ++i){
                 for (int j = 0; j < tempArr.length; ++j){
                     if (i != j){
@@ -67,8 +72,80 @@ public class BullsAndCowsGame {
         return tempArr;
     }
 
+    // ввод числа пользователя
+    private int[] getUserEnteredNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число длинной " + numberLength + " цифр");
+        String userNumString = scanner.nextLine(); // читается вся строка, введенная пользователем
+        int[] tempArr = new int[numberLength];
+
+        // проверяем, что введенная строка содержит только цифры и ее длина совпадает с ожидаемой длиной числа
+        if (userNumString.matches("\\d{" + numberLength + "}")) { // регулярное выражение означает, что строка должна состоять из numberLength подряд
+            // Преобразуем строку в массив цифр
+            for (int i = 0; i < numberLength; ++i) {
+                // метод, к. преобраз. символ в числ. знач., представляющ. этот символ, иначе -1
+                tempArr[i] = Character.getNumericValue(userNumString.charAt(i)); // символ на позиции i в строке input
+            }
+        } else {
+            // если введенная строка не соответствует ожидаемой длине или содержит нецифровые символы, выдаем сообщение об ошибке
+            System.out.println("Ошибка! Введите число из " + numberLength + " цифр.");
+            // рекурсивно вызываем этот метод снова, чтобы попросить пользователя ввести число снова
+            tempArr = getUserEnteredNumber();
+        }
+
+        return tempArr;
+    }
+
+
+    // проверка числа пользователя
+    private void checkUserNumber(int[] userEnteredNumber) {
+        int bulls = 0;
+        int cows = 0;
+        for (int i = 0; i < numberLength; ++i){
+            if (userEnteredNumber[i] == computerGuessedNumber[i]){
+                ++bulls;
+            } else {
+                for (int j = 0; j < numberLength; ++j) {
+                    if (userEnteredNumber[i] == computerGuessedNumber[j]) {
+                        ++cows;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // выводим результат проверки
+        if (bulls == numberLength) {
+            System.out.println("Поздравляем! Вы угадали число!");
+            System.exit(0); // завершаем программу после выигрыша
+        } else {
+            System.out.println("Результат: " + bulls + " бык(ов), " + cows + " коров(а).");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.print("Привет! Давай сыграем в игру \"Быки и коровы\"? Компьютер уже загадал число, попробуй отгадать!");
-        // Добавить описание правил игры
+        System.out.println("Привет! Давай сыграем в игру \"Быки и коровы\"? Компьютер уже загадал число, попробуй отгадать!");
+        BullsAndCowsGame game = new BullsAndCowsGame();
+        game.getCompNum();
+
+        // Вывод загаданного компьютером числа
+        System.out.print("Загаданное компьютером число: ");
+        game.printCompNum(game.computerGuessedNumber);
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean continueGame = true;
+        do {
+            int[] userEnteredNumber = game.getUserEnteredNumber();
+            game.checkUserNumber(userEnteredNumber);
+
+            System.out.println("Хотите сыграть еще раз? (Y/N)");
+            String choice = scanner.nextLine();
+            if (!choice.equalsIgnoreCase("Y")) {
+                continueGame = false;
+            }
+        } while (continueGame);
+
+        System.out.println("Спасибо за игру!");
     }
 }
